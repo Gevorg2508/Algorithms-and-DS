8bfaf207ec5785032f27cb85600ee86a0776825f
Gevorg Harutyunyan <gev.harutyunyan.96@gmail.com>
2022-02-17 17:07:46 +0400

    feat: i have done some changes and before that Sona shows me the right versionfor soolving this code

--- MinimalPositive.2.js
+++ MinimalPositive.2.js
@@ -1,16 +1,41 @@
-let newArr = [45, -9, 15, 5, -78];
+const newArr = [-5, -9, -111, -1000, -7];
 
 let index = 0;
 let array = [];
-function minimal(arr) {
+function minimal(arr, ) {
     if (arr[index] >= 0) {
         array.push(arr[index]);
+    }else{
+        return -1;
     }
     if (index > arr.length) {
         index = 0;
-        return array.sort((a,b) => a-b)[0];
+        return Math.min(...array);
     }
     index++;
     return minimal(arr);
 }
 console.log(minimal(newArr));
+
+
+// function findMinEl(arr, minEl = Infinity) {
+//     console.log(arr)
+//     const newArr = [...arr]
+
+//     if(newArr.length === 0) {
+//        if (minEl === Infinity) return -1;
+//        else return minEl ;  
+//     }
+
+//     const [firstElm, ...partArr] = newArr;
+
+//     if(firstElm >= 0 && firstElm < minEl){
+//         minEl = firstElm;
+//     }
+
+//     return findMinEl(partArr, minEl)
+
+// }
+
+// console.log(findMinEl(arr))
+
