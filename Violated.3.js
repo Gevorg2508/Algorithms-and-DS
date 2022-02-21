4a364d721ec6b9b9e2c1491ac06773a6357a9eaa
Gevorg Harutyunyan <gev.harutyunyan.96@gmail.com>
2022-02-21 08:33:40 +0400

    feat: the final version of this code

--- Violated.3.js
+++ Violated.3.js
@@ -1,14 +1,18 @@
-let index = 0;
-function violated(arr){
-    if(arr[index] > arr[index+1]){
-        return index + 1;
-    }else{
-        if(index > arr.length){
-            index = 0;
-            return -1;
+// let index = 0;
+function violated(arr, index = 0) {
+    if (Array.isArray(arr)) {
+        if (arr[index] > arr[index + 1]) {
+            return index + 1;
+        } else {
+            if (index > arr.length) {
+                index = 0;
+                return -1;
+            }
         }
-    }
         index++;
-        return violated(arr);
+        return violated(arr, index);
+    }else{
+        return "input right value";
+    }
 }
-console.log(violated([-9, -4, -4, 3, 12, 4, 5]))
+console.log(violated([2, 12, 15, 48, 64]))
