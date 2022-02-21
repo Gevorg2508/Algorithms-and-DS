470d458f01cad6f074fc77d8eeab4d06c60b214b
Gevorg Harutyunyan <gev.harutyunyan.96@gmail.com>
2022-02-21 08:38:36 +0400

    feat: the last version of this code

--- rmFirstElem.4.js
+++ rmFirstElem.4.js
@@ -1,12 +1,14 @@
-let length = 0;
-function rmElem(arr){
-   
+function rmElem(arr, length = 0){
+    if(Array.isArray(arr)){
    if(length >= arr.length){
        return arr;
    }else{
        length = arr.length;
    }
    const [firstElem, ...array] = arr;
-   return rmElem(array);
+   return rmElem(array, length);
+    }else{
+        return "input right value";
+    }
 }
 console.log(rmElem([1, 2, 3, 4, 5]))
