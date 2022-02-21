c350d792984cb8f162c1daa74d6bde74a9207ab8
Gevorg Harutyunyan <gev.harutyunyan.96@gmail.com>
2022-02-21 08:15:21 +0400

    feat: here is the right version of this task

--- OddOrNot.1.js
+++ OddOrNot.1.js
@@ -1,17 +1,22 @@
-let index = 0;
-function oddOrNot(num) {
-  let number = num.toString().split("");
-  if (index === number.length && number[number.length - 1] % 2 !== 0) {
-    return true;
+function oddOrNot(num, index = 0) {
+  if (!isNaN(num)) {
+    let number = num.toString().split("");
+    if (index === number.length && number[number.length - 1] % 2 !== 0) {
+      return true;
+    }
+    if (number[index] % 2 === 0) {
+      index = 0;
+      return false;
+    }
+    index++;
+    return oddOrNot(num, index);
+  }else{
+    return "input right value";
   }
-  if (number[index] % 2 === 0) {
-    index = 0;
-    return false;
-  }
-      index++;
-      return oddOrNot(num);
 }
-console.log(oddOrNot(1111111111111111111111111111111111111111111111111111112n));// BigInt
-console.log(oddOrNot(1111111));
-console.log(oddOrNot(11111112));
+// console.log(oddOrNot(1111111111111111111111111111111111111111111111111111112n));// BigInt
+// console.log(oddOrNot(1111111));
+// console.log(oddOrNot(11111112));
+console.log(oddOrNot(1 13));
+
 
